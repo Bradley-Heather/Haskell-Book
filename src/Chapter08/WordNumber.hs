23b9ecb3ab249fd4622554c2b841d5cecd8c4768
Bradley Heather <bradleymheather@gmail.com>
2021-10-06 11:49:04 -0400

    Create WordNumber.hs
    Changed module name

---
+++ src/Chapter08/WordNumber.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module WordNumber where
+
+import Data.List
+import Data.Char
+
+data DividedResult = Result (Integer, Integer) | DividedByZero 
+     deriving (Show, Eq)
+
+divideBy :: Integer -> Integer -> DividedResult
+divideBy 0 _ = DividedByZero
+divideBy _ 0 = DividedByZero
+divideBy num denom =  Result (go num denom 0)
+   where go n d count 
+          | n < d = (count, n)
+          | otherwise =
+                go (n - d) d (count + 1)
+
+mc91 :: Integer -> Integer
+mc91 n 
+   | n > 100   = n - 10
+   | otherwise = 91 
+
+digitToWord :: Int -> String 
+digitToWord n = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"] !! n
+
+digits :: Int -> [Int]
+digits n = map digitToInt $ show n
+
+wordNumber :: Int -> String
+wordNumber n = intercalate "-" . map digitToWord $ digits n
+

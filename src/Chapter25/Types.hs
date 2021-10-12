a61d8447dc56eae99314660d41e5697c2a342d05
Bradley Heather <bradleymheather@gmail.com>
2021-10-12 16:09:18 -0400

    Create Types.hs

---
+++ src/Chapter25/Types.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module Types where 
+
+
+newtype One f a = One (f a)
+  deriving (Eq, Show)
+
+instance Functor f => Functor (One f) where
+  fmap f (One fa) = One $ fmap f fa
+----------------------------
+
+newtype Three f g h a = Three (f (g (h a)))
+  deriving (Eq, Show)
+
+instance (Functor f, Functor g, Functor h) => Functor (Three f g h) where
+  fmap f (Three fgha) = Three $ (fmap . fmap . fmap) f fgha
+-----------------------------
+
+newtype Compose f g a = Compose { getCompoose :: f (g a) }
+   deriving (Eq, Show)
+
+instance (Functor f, Functor g) => Functor (Compose f g) where 
+    fmap f (Compose fga) = Compose $ (fmap. fmap) f fga 
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where 
+    pure :: a -> Compose f g a 
+    pure a = Compose $ (pure . pure) a
+
+    (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b 
+    Compose f <*> Compose a = Compose $ (<*>) <$> f <*> a

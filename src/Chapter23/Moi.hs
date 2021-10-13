5200a4274182aee3e41dde32fc45fdfa8a8396c9
Bradley Heather <bradleymheather@gmail.com>
2021-10-13 10:49:09 -0400

    Update Moi.hs

--- src/Chapter23/Moi.hs
+++ src/Chapter23/Moi.hs
@@ -6,7 +6,8 @@ newtype Moi s a = Moi { runMoi :: s -> (a, s) }
 
 instance Functor (Moi s) where 
     fmap :: (a -> b) -> Moi s a -> Moi s b
-    fmap f (Moi g) = undefined 
+    fmap f (Moi g) = Moi $ \s -> let (a, s1) = g s 
+                                 in (f a, s1)
 
 instance Applicative (Moi s) where 
     pure :: a -> Moi s a

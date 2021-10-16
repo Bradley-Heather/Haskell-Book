a7e3f2b6f97fddc96e541463c70bb93358a822ea
Bradley Heather <bradleymheather@gmail.com>
2021-10-16 18:40:22 -0400

    Update Scotty.hs

--- src/Chapter26/Scotty.hs
+++ src/Chapter26/Scotty.hs
@@ -3,13 +3,21 @@
 module Scotty where 
 
 import Web.Scotty
+import Web.Scotty.Internal.Types (ActionT(..))
 
-import Data.Monoid (mconcat)
+import Data.Monoid               (mconcat)
 import Control.Monad.Trans.Class
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Reader
 
 main = scotty 3000 $ do 
     get "/:word" $ do 
         beam <- param "word"
+        -- (ActionT . (ExceptT . fmap Right) . ReaderT . const 
+        --   . \m -> StateT (\s -> do 
+        --                       a <- m
+        --                       return (a, s) ))
         lift (putStrLn "hello")
 
         html $ 

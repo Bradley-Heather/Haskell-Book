aa0d9d0fa2977ff6c4b3f6933a8a1984b547645d
Bradley Heather <bradleymheather@gmail.com>
2021-11-03 10:33:36 -0400

    Added chapter 30

---
+++ src/Chapter30/WritingToFiles/writepls.hs
@@ -0,0 +1,15 @@
+module Main where 
+
+import Control.Exception 
+import Data.Typeable 
+
+handler :: SomeException -> IO ()
+handler (SomeException e) = do 
+    putStrLn ("Running main caused an error!\ 
+             \It was: " ++ show e)
+    writeFile "bbb" "hi"
+
+main :: IO ()
+main = do 
+    writeFile "zzz" "hi"
+       `catch` handler 

914031df2bd58390e4cd44f6ebd8ea0dd94123b4
Bradley Heather <bradleymheather@gmail.com>
2021-11-03 11:30:34 -0400

    refractor

--- src/Chapter30/ourExceptions.hs
+++ src/Chapter30/ourExceptions.hs
@@ -2,19 +2,19 @@ module OurExceptions where
 
 import Control.Exception 
 
-data NotDivThree = NotDivThree 
+data NotDivThree = NotDivThree Int
    deriving (Eq, Show) 
 
 instance Exception NotDivThree 
 
-data NotEven = NotEven 
+data NotEven = NotEven Int
    deriving (Eq, Show)
 
 instance Exception NotEven 
 
 evenAndThreeDiv :: Int -> IO Int 
 evenAndThreeDiv i 
-   | rem i 3 /= 0 = throwIO NotDivThree
-   | odd i = throwIO NotEven 
+   | rem i 3 /= 0 = throwIO (NotDivThree i)
+   | odd i = throwIO (NotEven i) 
    | otherwise = return i 
 
